2ceddfc18e8628011d78e9c48703f409fea915c0
Matheus Eiki <matheuseikikimura@gmail.com>
2026-02-04 21:48:58 -0300

    mudanças sistema de inventário

--- Inventario/inventario.gd
+++ Inventario/inventario.gd
@@ -1,47 +1,22 @@
 class_name Inventory
 extends Node3D
 
-@export var inventory_items: Array = []
-@export var spawn_point: Marker3D #Vou usar isso para spawnar o modelo 3d do item a ser exibido
-@export var name_label: Label
-@export var description_label: RichTextEffect
+@export var lista_de_itens: Array
+@onready var item_holder = $Itemholder
 
-var current_index: int = 0
-var current_model: Node = null
+var indice_atual: int = 0
+var item_instanciado: Node = null
 
 func _ready() -> void:
-	update_display()
-	GameManager.inventory_camera = $SubViewportContainer/SubViewport/Camera3D
-	GameManager.inventory_ui = $SubViewportContainer/SubViewport/CanvasLayer
+	exibir_item(indice_atual)
 
+#Função para navegar no inventário
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_right"): #Tecla D
-		change_item(1)
-	elif event.is_action_pressed("ui_left"):
-		change_item(-1)
+	if InputEvent.is_action_just_pressed("ui_rigth"): mudar_indice(1)
+	elif InputEvent.is_action_just_pressed(): mudar_indice(-1)
 
-
-func change_item(direction: int) -> void:
-	if inventory_items.size() == 0: return
-	
-	#Lógica Cíclica
-	current_index = (current_index + direction + inventory_items.size()) % inventory_items.size()
-	update_display()
-	
-func update_display() -> void:
-	if inventory_items.size() == 0: return
-	var active_item = inventory_items[current_index]
-	
-	# Atualiza Textos
-	name_label.text = active_item.name
-	description_label.text = active_item.description
-	
-	# Limpa o modelo anterior
-	if current_model:
-		current_model.queue_free()
-	
-	# Instancia o novo modelo 3D
-	if active_item.scene:
-		current_model = active_item.scene.instantiate()
-		spawn_point.add_child(current_model)
-		# Adicionar animação dps aqui
+func mudar_indice(direcao:int) -> void:
+	indice_atual += direcao
+	# Sistema circular, se passar do fim volta para ao início
+	if indice_atual >= lista_de_itens(): indice_atual = 0
+	elif indice_atual < 0: indice_atual = lista_de_itens.size() - 1
