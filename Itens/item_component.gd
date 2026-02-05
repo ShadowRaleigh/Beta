d63ab8cd39c1616d419f963cfe8ec8eeeaaec4f8
ShadowRaleigh <58346685+ShadowRaleigh@users.noreply.github.com>
2026-02-05 00:17:50 -0300

    Bug fix item model
    Agora os modelos dos itens aparecem quando o jogo é carregado

--- Itens/item_component.gd
+++ Itens/item_component.gd
@@ -5,6 +5,10 @@ extends Node
 @export var idata:ItemData
 @export_tool_button("Load Model","Camera3DDarkBackground") var model_visualizer = visualizer
 
+func _ready() -> void:
+	var instance = idata.model.instantiate()
+	get_parent().add_child.call_deferred(instance)
+
 func visualizer():
 	var instance = idata.model.instantiate()
 	add_child(instance)
--- Player/Item Collector/collector.gd
+++ Player/Item Collector/collector.gd
@@ -2,16 +2,9 @@ extends Node3D
 
 @onready var detector: RayCast3D = $Detector
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	if detector.get_collider() and Input.is_action_just_pressed("collect"):
 		var collected_object:Node3D = detector.get_collider()
 		GameManager.inventory_list.append(collected_object.duplicate())
 		collected_object.queue_free()
-
-	print(GameManager.inventory_list)
